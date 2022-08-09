321cf93738baf5842379729f6e95b6e3ea4910e7
Cristiano Hoshikawa <hoshikawa@uol.com.br>
2022-08-08 23:06:47 -0300

    zipkin

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const axios = require("axios");
+const zipkinInstrumentationAxios = require("zipkin-instrumentation-axios");
+const {Annotation, InetAddress} = require('zipkin');
 
 var redis = require('redis');
 
@@ -24,45 +26,40 @@ router.get('/cep', function (req, res, next) {
     var client = redis.createClient("6379", "141.148.39.105");
     client.auth("P@ssw0rd");
     client.on('connect', function() {
-
-        //zipkin
-        ctxImpl.scoped(() => {
-            tracer.recordServiceName('REDIS conectado');
-        });
-
         console.log('REDIS conectado');
     });
 
     client.get(req.query.cep, function(err, reply) {
         if (reply != null) {
-
-            console.log('Resposta via REDIS');
-
             //zipkin
-            ctxImpl.scoped(() => {
-                tracer.recordServiceName('Resposta via REDIS');
+            const originalId = tracer.createChildId();
+            tracer.letId(originalId, () => {
+                tracer.recordBinary('error', "Step 1");
+                tracer.recordAnnotation(new Annotation.ClientRecv());
+                tracer.recordAnnotation(new Annotation.ClientSend());
             });
 
+            tracer.local('Resposta via REDIS', () => {
+                console.log('Resposta via REDIS');
+            });
             console.log(reply);
             //Canarian OK POD
             res.send(JSON.parse(reply));
             //Canarian NOK POD
             //res.send(reply);
             res.end();
-
         } else {
             const url = "https://viacep.com.br/ws/" + req.query.cep + "/json/";
 
-            axios.get(url).then(function(resposta) {
+            // Add axios instrumentation
+            const zipkinAxios = zipkinInstrumentationAxios(axios, { tracer, serviceName: "Consultou VIACEP" });
+
+            //axios.get(url).then(function(resposta) {
+            zipkinAxios.get(url).then(function(resposta) {
                 res.send(resposta.data);
                 res.end();
                 console.log('Consultou VIACEP');
                 console.log(resposta.data);
-
-                //zipkin
-                ctxImpl.scoped(() => {
-                    tracer.recordServiceName('Consultou VIACEP');
-                });
 
                 client.set(req.query.cep, JSON.stringify(resposta.data));
             })
